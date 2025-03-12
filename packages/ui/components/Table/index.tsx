bd86ffdc5b4c87954057fc30b891d1c378bf1abb
minwoo129 <key0129mw@naver.com>
2025-03-12 10:44:02 +0900

    refactor: 테이블 컴포넌트 행에서 발생하는 이벤트 처리 props 구조 수정 - #3
    - 한 단계 더 타고 들어가야 하는 이벤트 처리 메서드를 props 최상단으로 끌어올림
    - 메서드 이름을 좀더 알아보기 쉽게 변경하고 설명을 위해 주석 추가

--- packages/ui/components/Table/index.tsx
+++ packages/ui/components/Table/index.tsx
@@ -6,11 +6,44 @@ import { TableObject, TableProps } from './types';
 const Table = <T extends TableObject>({
   datas,
   columns,
+  onRowItemClick,
+  onRowItemDoubleClick,
+  onRowItemContextMenu,
+  onRowItemMouseEnter,
+  onRowItemMouseLeave,
   ...props
 }: TableProps<T>) => {
   const { convertedColumns } = useTable<T>({ columns, datas });
   return (
-    <AntdTable<T> dataSource={datas} columns={convertedColumns} {...props} />
+    <AntdTable<T>
+      dataSource={datas}
+      columns={convertedColumns}
+      onRow={(record, rowIndex) => {
+        return {
+          onClick: (e) => {
+            if (!onRowItemClick) return;
+            onRowItemClick(e, record, rowIndex);
+          },
+          onDoubleClick: (e) => {
+            if (!onRowItemDoubleClick) return;
+            onRowItemDoubleClick(e, record, rowIndex);
+          },
+          onContextMenu: (e) => {
+            if (!onRowItemContextMenu) return;
+            onRowItemContextMenu(e, record, rowIndex);
+          },
+          onMouseEnter: (e) => {
+            if (!onRowItemMouseEnter) return;
+            onRowItemMouseEnter(e, record, rowIndex);
+          },
+          onMouseLeave: (e) => {
+            if (!onRowItemMouseLeave) return;
+            onRowItemMouseLeave(e, record, rowIndex);
+          },
+        };
+      }}
+      {...props}
+    />
   );
 };
 
--- packages/ui/components/Table/types.ts
+++ packages/ui/components/Table/types.ts
@@ -5,9 +5,64 @@ import { ColumnProps } from 'antd/es/table';
 export type TableObject = Record<string | number | symbol, any>;
 
 export interface TableProps<T extends TableObject>
-  extends Omit<ComponentProps<typeof AntdTable<T>>, 'columns' | 'dataSource'> {
+  extends Omit<
+      ComponentProps<typeof AntdTable<T>>,
+      'columns' | 'dataSource' | 'onRow'
+    >,
+    TableRowEvents<T> {
+  /**
+   * 테이블 데이터
+   */
   datas: T[];
+  /**
+   * 테이블 컬럼
+   */
   columns: ColumnStructure<T>;
+}
+
+export interface TableRowEvents<T extends TableObject> {
+  /**
+   * 행(데이터) 클릭 이벤트
+   */
+  onRowItemClick?: (
+    e: React.MouseEvent<any, MouseEvent>,
+    record: T,
+    rowIndex: number | undefined,
+  ) => void;
+  /**
+   * 행(데이터) 더블클릭 이벤트
+   */
+  onRowItemDoubleClick?: (
+    e: React.MouseEvent<any, MouseEvent>,
+    record: T,
+    rowIndex: number | undefined,
+  ) => void;
+  /**
+   * 행(데이터) 컨텍스트 메뉴(마우스 우클릭 시 발생) 이벤트
+   */
+  onRowItemContextMenu?: (
+    e: React.MouseEvent<any, MouseEvent>,
+    record: T,
+    rowIndex: number | undefined,
+  ) => void;
+  /**
+   * 행(데이터) 마우스 호버링 이벤트
+   * - 마우스가 해당 행에 진입했을 때 발생
+   */
+  onRowItemMouseEnter?: (
+    e: React.MouseEvent<any, MouseEvent>,
+    record: T,
+    rowIndex: number | undefined,
+  ) => void;
+  /**
+   * 행(데이터) 마우스 호버링 이벤트
+   * - 마우스가 해당 행에서 빠져나갔을 때 발생
+   */
+  onRowItemMouseLeave?: (
+    e: React.MouseEvent<any, MouseEvent>,
+    record: T,
+    rowIndex: number | undefined,
+  ) => void;
 }
 
 export type ArrayElement<T> = T extends (infer U)[] ? U : never;
@@ -24,7 +79,13 @@ type ColumnStructure<T extends TableObject> = {
 };
 
 export type UseTableArgs<T extends TableObject> = {
+  /**
+   * 테이블 데이터
+   */
   datas: T[];
+  /**
+   * 테이블 컬럼
+   */
   columns: ColumnStructure<T>;
 };
 
