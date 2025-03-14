b8201bb38b6b25118a900ca78185f55554fe9455
minwoo129 <key0129mw@naver.com>
2025-03-14 21:06:39 +0900

    feat: Select, Switch 컴포넌트 추가 - #3
    - 기능 동작을 위한 hook 동시 개발
    - 커밋 메세지 오타로 리셋 후 두 기능개발 내용 동시 커밋

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -11,6 +11,10 @@ export { default as Chart } from './src/components/Chart';
 export { default as SearchInput } from './src/components/SearchInput';
 export { default as Checkbox } from './src/components/Checkbox';
 export { default as Radio } from './src/components/Radio';
+export { default as Select } from './src/components/Select';
+export { default as Switch } from './src/components/Switch';
 // ======================================= hooks =======================================
 export { default as useAdminUITable } from './src/hooks/useAdminUITable';
 export { default as useAdminUIRadio } from './src/hooks/useAdminUIRadio';
+export { default as useAdminUISelect } from './src/hooks/useAdminUISelect';
+export { default as useAdminUISwitch } from './src/hooks/useAdminUISwitch';
---
+++ packages/ui/src/hooks/useAdminUISelect.tsx
@@ -0,0 +1,42 @@
+import React, { ComponentProps, useState } from 'react';
+import Select from '../components/Select';
+import { DefaultOptionType } from 'antd/es/select';
+
+interface SelectOptionType<T extends string | number>
+  extends Omit<DefaultOptionType, 'value'> {
+  value: T;
+}
+
+interface SelectComponentProps<T extends string | number>
+  extends Omit<
+    ComponentProps<typeof Select<T>>,
+    'defaultValue' | 'value' | 'options'
+  > {
+  options: SelectOptionType<T>[];
+}
+
+type UseAdminUISelectRetType<T extends string | number> = [
+  T | undefined,
+  (args: SelectComponentProps<T>) => React.ReactNode,
+];
+
+const useAdminUISelect = <T extends string | number>(
+  defaultValue?: T,
+): UseAdminUISelectRetType<T> => {
+  const [value, setValue] = useState(defaultValue);
+
+  const SelectComponent = ({ options, ...props }: SelectComponentProps<T>) => {
+    return (
+      <Select
+        value={value}
+        options={options}
+        onChange={(value) => setValue(value)}
+        {...props}
+      />
+    );
+  };
+
+  return [value, SelectComponent];
+};
+
+export default useAdminUISelect;
