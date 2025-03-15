10058e8570bbf0be5fd97bcfd69b31c480c137fc
minwoo129 <key0129mw@naver.com>
2025-03-15 13:14:15 +0900

    style: 메인 프로젝트 배경화면 배경색 변경, ui 개발 테스트 용 임시 CSS 추가 - #3

--- apps/main-app/src/App.tsx
+++ apps/main-app/src/App.tsx
@@ -1,27 +1,13 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+//import reactLogo from './assets/react.svg';
+//import viteLogo from '/vite.svg';
 import './App.css';
+//import { Table } from '@admin/ui';
+//import { Table } from '@admin/ui';
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React(Main)</h1>
-      <div className="card">
-        {/* <Button type="primary" color="green">
-          Test
-        </Button> */}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="tableTestGrid"></div>
     </>
   );
 }
