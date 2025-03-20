e4acd9d36e5fc15b807a0854d72cd822a4d5b250
minwoo129 <key0129mw@naver.com>
2025-03-20 17:17:17 +0900

    feat: #18 메뉴 열림 여부 초기값 변경

--- packages/ui/src/components/Menu/index.tsx
+++ packages/ui/src/components/Menu/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Menu as AntdMenu } from 'antd';
 import type { MenuProps } from './types';
 
-const Menu = ({ items, onMenuClick, isMenuOpened }: MenuProps) => {
+const Menu = ({ items, onMenuClick, isMenuOpened = true }: MenuProps) => {
   return (
     <AntdMenu
       mode="inline"
       theme="dark"
+      style={{ width: isMenuOpened ? '256px' : 'fit-content' }}
       items={items}
       onClick={onMenuClick}
-      inlineCollapsed={isMenuOpened}
+      inlineCollapsed={!isMenuOpened}
     />
   );
 };
