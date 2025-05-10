82ae4844f0cbb7c72afabf6f814bbfb50478263b
minwoo129 <key0129mw@naver.com>
2025-05-10 13:13:16 +0900

    feat: #29 storybook Calendar 문서 중간 작성

--- packages/ui/src/components/Calendar/types.ts
+++ packages/ui/src/components/Calendar/types.ts
@@ -4,11 +4,16 @@ import type {
   SelectInfo,
 } from 'antd/es/calendar/generateCalendar';
 import type { Dayjs } from 'dayjs';
+import { CellRenderInfo } from 'rc-picker/lib/interface';
 
 export interface CalendarProps
-  extends Omit<AntdCalendarProps<Dayjs>, 'onSelect' | 'onPanelChange'> {
+  extends Omit<
+    AntdCalendarProps<Dayjs>,
+    'onSelect' | 'onPanelChange' | 'cellRender'
+  > {
   /** 날짜가 변경됬을 때 이벤트 */
   onSelect?: (date: Dayjs, selectInfo: SelectInfo) => void;
   /** 현재 보여지는 화면이 변경됬을 때 이벤트 */
   onPanelChange?: (date: Dayjs, mode: CalendarMode) => void;
+  cellRender?: (date: Dayjs, info: CellRenderInfo<Dayjs>) => React.ReactNode;
 }
